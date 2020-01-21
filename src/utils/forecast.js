ac597f9baac8c216557f5cd1421b1237c4977e4d
Prakash Selvaraju <pselvaraju@changehealthcare.com>
2020-01-20 22:01:15 -0600

    Added maximum and minimum temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,18 +8,13 @@ const forecast = (lat, long, callback) => {
         if(error) {
             callback("Unable to connect to weather service.", undefined)
         } else if (body.error) {
-            //console.log(response)
             callback("Unable to find location.", undefined)
         } else {
             const { currently, daily } = body
             const { temperature, precipProbability } = currently
-            const { summary } = daily.data[0]
-            const forecastString = summary + ", It is currently " + temperature + " degrees out. There is a " + precipProbability + "% chance of rain."
-            // callback(undefined, {
-            //     summary: daily[0].summary,
-            //     temperature: currently.temperature,
-            //     precipProbability: currently.precipProbability,                  
-            // })
+            const { summary, temperatureHigh, temperatureLow } = daily.data[0]
+            console.log("daily ", daily)
+            const forecastString = summary + ", It is currently " + temperature + " degrees out." + " The maximum temperature is " + temperatureHigh + " degrees and the minimum is " + temperatureLow + " degrees. There is a " + precipProbability + "% chance of rain."
             callback(undefined, forecastString)
         }
     })
